a580052f002a2cbb4143ae77aa9e9cc494b5e466
Andrew Ogle <andrewryanogle@gmail.com>
2021-06-12 00:19:57 -0700

    init

--- componets/SignUpPage/LoginPage.js
+++ componets/SignUpPage/LoginPage.js
@@ -1,31 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React from 'react';
 import { StyleSheet, Text, View } from "react-native";
+import LoginStyles from "./LoginStyles";
 import { useFonts, Lobster_400Regular} from "@expo-google-fonts/lobster";
 import AppLoading from 'expo-app-loading';
 
-import LoginPage from "./componets/SignUpPage/LoginPage";
-
-export default function App() {
+const LoginPage = (props) => {
   const [fontsLoaded] = useFonts( {Lobster_400Regular});
   if(!fontsLoaded){
     return <AppLoading/>
   }else{
-
-
     return (
       <View style={styles.container}>
-        <LoginPage />
+        <Text style={styles.text}>HonieDo!</Text>
       </View>
     );
   }
 }
+;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create(LoginStyles);
+
+export default LoginPage;
---
+++ componets/SignUpPage/LoginStyles.js
@@ -0,0 +1,14 @@
+const LoginStyles = {
+  container: {
+    backgroundColor: "powderblue",
+    flex: 1,
+    width: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Lobster_400Regular'
+  }
+};
+
+export default LoginStyles;
